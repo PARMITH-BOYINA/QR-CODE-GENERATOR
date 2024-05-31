d29d0ae1e5038ee0e810c8d446a88e3f8d72ee9e
PARMITH-BOYINA <parmithsai@gmail.com>
2024-05-31 20:41:25 +0530

    qrcode-gen

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'; 
+import './App.css'; 
+
+function App() { 
+const [temp, setTemp] = useState(""); 
+const [word, setWord] = useState(""); 
+const [qrCode, setQrCode] = useState(""); 
+
+useEffect(() => { 
+	setQrCode 
+(`http://api.qrserver.com/v1/create-qr-code/?data=${word}!&`); 
+}, [word]); 
+
+function handleClick() { 
+	setWord(temp); 
+} 
+
+return ( 
+	<div className="App"> 
+	<h1>QR Code Generator</h1> 
+	<div className="input-box"> 
+		<div className="gen"> 
+		<input type="text" onChange={ 
+			(e) => {setTemp(e.target.value)}} 
+			placeholder="Enter text to encode" /> 
+		<button className="button"
+			onClick={handleClick}> 
+			Generate 
+		</button> 
+		</div> 
+		<div className="extra"> 
+		</div> 
+	</div> 
+	<div className="output-box"> 
+		<img src={qrCode} alt="" /> 
+		<a href={qrCode} download="QRCode"> 
+		<button type="button">Download</button> 
+		</a> 
+	</div> 
+	</div> 
+); 
+} 
+
+export default App;
